1feb1762dfe0ac21e7eb8bf610777732d502f927
Josh Morris <jmo0624@gmail.com>
2020-01-23 01:49:28 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
-      t.sting :theme
+      t.string :theme
       t.float :price
       t.boolean :family_friendly
       t.datetime :opening_date
