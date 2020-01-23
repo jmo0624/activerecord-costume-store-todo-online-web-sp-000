fc878b64a76e9399bcc0b1bcf5acdde91f11a26a
Josh Morris <jmo0624@gmail.com>
2020-01-23 01:33:06 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,5 +11,12 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
   
   def change
-    create_table 
+    create_table :costumes do |t|
+      t.string :name,
+      t.float :price,
+      t.string :size,
+      t.string :image_url,
+      :timestamps
+    end
+    
 end
