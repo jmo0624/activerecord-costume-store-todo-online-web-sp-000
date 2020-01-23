676411d8b5c92301b85ca198d9210eec255c5643
Josh Morris <jmo0624@gmail.com>
2020-01-23 01:39:22 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,5 +6,7 @@ class CreateCostumeStore < ActiveRecord::Migration[5.1]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.string
 end
